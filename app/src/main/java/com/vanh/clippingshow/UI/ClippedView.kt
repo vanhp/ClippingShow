c65b411b929c4ed35a2eb347bb8316b7f6aa71e5
vanh <vanhphom@gmail.com>
2020-07-11 01:10:44 -0700

    draw the first clipping rectangle

--- app/src/main/java/com/vanh/clippingshow/UI/ClippedView.kt
+++ app/src/main/java/com/vanh/clippingshow/UI/ClippedView.kt
@@ -1,6 +1,8 @@
 package com.vanh.clippingshow.UI
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -37,6 +39,73 @@ class ClippedView @JvmOverloads constructor(context: Context,attrs:AttributeSet?
     private val rowThree = rowTwo + rectInset + clipRectBottom
     private val rowFour = rowThree + rectInset + clipRectBottom
     private val textRow = rowFour + (1.5f * clipRectBottom)
+
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        drawClippedRectangle(canvas)
+//        drawBackAndUnclippedRectangle(canvas)
+//        drawDifferenceClippingExample(canvas)
+//        drawCircularClippingExample(canvas)
+//        drawIntersectionClippingExample(canvas)
+//        drawCombinedClippingExample(canvas)
+//        drawRoundedRectangleClippingExample(canvas)
+//        drawOutsideClippingExample(canvas)
+//        drawSkewedTextExample(canvas)
+//        drawTranslatedTextExample(canvas)
+        // drawQuickRejectExample(canvas)
+    }
+    private fun drawClippedRectangle(canvas: Canvas) {
+        canvas.clipRect(clipRectLeft,clipRectTop, clipRectRight,clipRectBottom)
+        canvas.drawColor(Color.WHITE)
+        paint.color = Color.RED
+        canvas.drawLine(clipRectLeft,clipRectTop, clipRectRight,clipRectBottom,paint)
+        paint.color = Color.GREEN
+        canvas.drawCircle(circleRadius,clipRectBottom - circleRadius, circleRadius,paint)
+        paint.color = Color.BLUE
+        // Align the RIGHT side of the text with the origin.
+        paint.textSize = textSize
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.drawText(context.getString(R.string.clipping), clipRectRight,textOffset,paint
+)
+    }
+
+    private fun drawTranslatedTextExample(canvas: Canvas?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun drawSkewedTextExample(canvas: Canvas?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun drawOutsideClippingExample(canvas: Canvas?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun drawRoundedRectangleClippingExample(canvas: Canvas?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun drawCombinedClippingExample(canvas: Canvas?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun drawIntersectionClippingExample(canvas: Canvas?) {
+
+    }
+
+    private fun drawCircularClippingExample(canvas: Canvas?) {
+
+    }
+
+    private fun drawDifferenceClippingExample(canvas: Canvas?) {
+
+    }
+
+    private fun drawBackAndUnclippedRectangle(canvas: Canvas?) {
+
+    }
 
 
 }
